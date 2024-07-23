22fa7906a0480bf1eb4d448c599d6a4afd020c92
Tumbl3Weed <calvin.nel00@gmail.com>
2024-07-23 13:06:24 +0200

    Added Ai KB entry.

--- js/start.js
+++ js/start.js
@@ -26,5 +26,23 @@ export default class QuizCardsPlugin extends React.PureComponent {
 
     }
 
+    /** AI knowledge base */
+    $ai_getKnowledgeBaseEntries = () => [
+
+        // Info
+        {
+            id: `${this.id}:quizcards`,
+            type: 'info',
+            name: 'Quiz Cards',
+            tags: 'quizcards, how to play quiz cards',
+            content: `
+                Quiz Cards is a fun way to learn and test your knowledge. You can play this game with others in the same world.
+                The person who picks a card has a certain time limit to explain the keywords/phrases on the card to the others without
+                saying the words on the card. They get points for every keywords/phrases they succesfully explain.
+            `
+        },
+
+    ]
+
   
 }   
